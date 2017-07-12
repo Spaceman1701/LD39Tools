6e6717ffdbb0f36c1925daf894a1aa7e0313393a
Ethan Hunter <fc.spaceman@gmail.com>
2017-07-12 17:49:31 -0600

    inital gradle project

---
+++ CreatorLib/src/main/java/org/x2a/TerminalApplication.java
@@ -0,0 +1,15 @@
+package org.x2a;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by ethan on 7/11/17.
+ */
+
+
+public class TerminalApplication {
+
+    private static Logger log = LogManager.getLogger(TerminalApplication.class);
+
+}
