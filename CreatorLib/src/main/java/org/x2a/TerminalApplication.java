2a6f74ae0794b0595e325f235e0068b585a646e4
Ethan Hunter <fc.spaceman@gmail.com>
2017-07-12 20:09:43 -0600

    add log4j and properly set up project

--- CreatorLib/src/main/java/org/x2a/TerminalApplication.java
+++ CreatorLib/src/main/java/org/x2a/TerminalApplication.java
@@ -1,7 +1,9 @@
 package org.x2a;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.apache.log4j.*;
+
+import java.io.IOException;
 
 /**
  * Created by ethan on 7/11/17.
@@ -9,7 +11,26 @@ import org.apache.logging.log4j.Logger;
 
 
 public class TerminalApplication {
+    private static final PatternLayout fileLayout = new PatternLayout("[%-5p] %d, %c - %m%n");
+    private static final PatternLayout consoleLayout = new PatternLayout("[%-5p] %c{5} - %m%n");
 
-    private static Logger log = LogManager.getLogger(TerminalApplication.class);
+    private static final Logger log = Logger.getLogger(TerminalApplication.class);
+
+    public static void main(String[] args) {
+        try {
+            configureLogging();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void configureLogging() throws IOException {
+        Logger.getRootLogger().addAppender(new FileAppender(fileLayout, "log.log"));
+        ConsoleAppender appender = new ConsoleAppender(consoleLayout);
+        appender.setThreshold(Level.INFO);
+        Logger.getRootLogger().addAppender(appender);
+
+        log.debug("logging configured");
+    }
 
 }
